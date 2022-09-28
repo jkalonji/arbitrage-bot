584b509f0eb519c3497fbad2c5f920a3c0463956
jkalonji <36740400+jkalonji@users.noreply.github.com>
2022-09-28 15:17:26 +0200

    Update Scout.py
    Tout dans une fonction

--- Scout.py
+++ Scout.py
@@ -12,21 +12,31 @@ def swapFromRune(inputAssetAmount, inputAssetBalance, outputAssetBalance):
     
 # ===================================== Main ===================================
 
-url = requests.get("https://midgard.thorchain.info/v2/pools")
-text = url.text
-data = json.loads(text)
 
-sethBalance    = float(data[6]['assetDepth']) 
-runeDepthseth  = float(data[6]['runeDepth'])
-ethBalance     = float(data[15]['assetDepth'])
-runeBalanceeth = float(data[15]['runeDepth'])
 
-firstSwapOutputNatToSynth   = swapToRune(1e8, ethBalance, runeBalanceeth)
-doubleSwapOutputNatToSynth = 1e-8*swapFromRune(firstSwapOutputNatToSynth , runeDepthseth, sethBalance)
-doubleSwapOutputNatToSynth
 
-firstSwapOutputSynthToNat   = swapToRune(1e8,sethBalance , runeDepthseth)
-doubleSwapOutputSynthToNat = 1e-8*swapFromRune(firstSwapOutputSynthToNat ,runeBalanceeth , ethBalance)
 
-print('doubleSwapOutputNatToSynth: ', doubleSwapOutputNatToSynth)
-print('doubleSwapOutputSynthToNat: ', doubleSwapOutputSynthToNat)
+
+def getSwapResults():
+    url = requests.get("https://midgard.thorchain.info/v2/pools")
+    text = url.text
+    data = json.loads(text)
+
+    sethBalance    = float(data[7]['assetDepth']) 
+    runeDepthseth  = float(data[7]['runeDepth'])
+    ethBalance     = float(data[16]['assetDepth'])
+    runeBalanceeth = float(data[16]['runeDepth'])
+
+    firstSwapOutputNatToSynth   = swapToRune(1e8, ethBalance, runeBalanceeth)
+    doubleSwapOutputNatToSynth = 1e-8*swapFromRune(firstSwapOutputNatToSynth , runeDepthseth, sethBalance)
+    doubleSwapOutputNatToSynth
+
+    firstSwapOutputSynthToNat   = swapToRune(1e8,sethBalance , runeDepthseth)
+    doubleSwapOutputSynthToNat = 1e-8*swapFromRune(firstSwapOutputSynthToNat ,runeBalanceeth , ethBalance)
+    if doubleSwapOutputNatToSynth>1 or doubleSwapOutputSynthToNat >1:
+        print('Opportunité trouvée !')
+        
+    return{"Nat to synth":doubleSwapOutputNatToSynth, "Synth to nat ":doubleSwapOutputSynthToNat}  # Dict contenant les résultats
+
+	
+print(getSwapResults())
