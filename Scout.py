96afaad6a8b85fcd6b929b9d1d2e385f2576d553
jkalonji <36740400+jkalonji@users.noreply.github.com>
2022-09-29 15:29:59 +0200

    Update Scout.py
    Ajout du try/except et inputAmount en variable

--- Scout.py
+++ Scout.py
@@ -22,21 +22,28 @@ def getSwapResults():
     text = url.text
     data = json.loads(text)
 
+# On ne  swap qu'entre ETH et ETH binance, car c'est la paire ETH qui rassemnle le plus de volume
     sethBalance    = float(data[7]['assetDepth']) 
     runeDepthseth  = float(data[7]['runeDepth'])
     ethBalance     = float(data[16]['assetDepth'])
     runeBalanceeth = float(data[16]['runeDepth'])
+    inputAmount = 10 # Vérifier l'unité de inputAmount
 
-    firstSwapOutputNatToSynth   = swapToRune(1e8, ethBalance, runeBalanceeth)
+    firstSwapOutputNatToSynth   = swapToRune(inputAmount*1e8, ethBalance, runeBalanceeth)
     doubleSwapOutputNatToSynth = 1e-8*swapFromRune(firstSwapOutputNatToSynth , runeDepthseth, sethBalance)
     doubleSwapOutputNatToSynth
 
-    firstSwapOutputSynthToNat   = swapToRune(1e8,sethBalance , runeDepthseth)
+    firstSwapOutputSynthToNat   = swapToRune(inputAmount*1e8,sethBalance , runeDepthseth)
     doubleSwapOutputSynthToNat = 1e-8*swapFromRune(firstSwapOutputSynthToNat ,runeBalanceeth , ethBalance)
-    if doubleSwapOutputNatToSynth>1 or doubleSwapOutputSynthToNat >1:
+    if doubleSwapOutputNatToSynth>inputAmount or doubleSwapOutputSynthToNat >inputAmount:
         print('Opportunité trouvée !')
         
     return{"Nat to synth":doubleSwapOutputNatToSynth, "Synth to nat ":doubleSwapOutputSynthToNat}  # Dict contenant les résultats
 
 	
-print(getSwapResults())
+	
+while(True):
+    try:
+        print(getSwapResults())
+    except:
+        pass
